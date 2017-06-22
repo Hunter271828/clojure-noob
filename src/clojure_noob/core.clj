82848da1ac3c64fc9ab1476164a76fa042da92d4
Hunter <long.hw@gmail.com>
2017-06-22 17:01:48 -0400

    First commit

---
+++ src/clojure_noob/core.clj
@@ -0,0 +1,10 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "I' ma little teapot..."))
+(defn train
+[]
+(println "Choo choo!"))
